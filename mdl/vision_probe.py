c2da3c669d3f1f642973cb888d8c2b70e1ef70b0
QuintinPope <popeq@oregonstate.edu>
2023-09-20 22:42:36 +0000

    Allowd transform size and training hyperparameters to be configurable

--- mdl/vision_probe.py
+++ mdl/vision_probe.py
@@ -12,6 +12,10 @@ class VisionProbe(Probe):
         self,
         num_features: int,
         num_classes: int = 2,
+        transform_size: int = 32,
+        learning_rate: float = 0.005,
+        momentum: float = 0.9,
+        weight_decay: float = 5e-4,
         model: str = "resnet18",
         device: str | torch.device | None = None,
         dtype: torch.dtype | None = None,
@@ -20,23 +24,26 @@ class VisionProbe(Probe):
 
         net = tv.models.get_model(model, num_classes=num_classes)
         self.net = net.to(device=device, dtype=dtype)  # type: ignore
+        self.learning_rate = learning_rate
+        self.momentum = momentum
+        self.weight_decay = weight_decay
         self.norm = tv.transforms.Normalize(
             (0.485, 0.456, 0.406),
             (0.229, 0.224, 0.225),
         )
         self.train_augmentor = tv.transforms.Compose(
             [
-                tv.transforms.RandomCrop(28),  # TODO: Make this configurable
+                tv.transforms.RandomCrop(transform_size),  # TODO: Make this configurable
             ]
         )
         self.test_augmentor = tv.transforms.Compose(
             [
-                tv.transforms.CenterCrop(28),
+                tv.transforms.CenterCrop(transform_size),
             ]
         )
 
     def build_optimizer(self) -> optim.Optimizer:
-        return optim.SGD(self.parameters(), lr=0.005, momentum=0.9, weight_decay=5e-4)
+        return optim.SGD(self.parameters(), lr=self.learning_rate, momentum=self.momentum, weight_decay=self.weight_decay)
 
     def augment_data(self, x: Tensor) -> Tensor:
         return self.train_augmentor(x) if self.training else self.test_augmentor(x)
