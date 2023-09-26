870a365404b6b9f3380099a71f177ea717fbf42d
Alex Mallen <atmallen8@gmail.com>
2023-09-26 00:28:29 +0000

    create sentiment dataset

--- experiments/concept_editing.py
+++ experiments/concept_editing.py
@@ -84,7 +84,7 @@ def train_model(
         device=X_train.device,
         dtype=torch.bfloat16,
     )
-    model.fit(X_train, Y_train, verbose=True)
+    model.fit(X_train, Y_train, verbose=True, max_epochs=200, early_stop_epochs=20)
     return model
 
 
