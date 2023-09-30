de35b2c2bf8f18d962192eba738ca50f51622854
Alex Mallen <atmallen8@gmail.com>
2023-09-30 05:22:19 +0000

    use vanilla LEACE in sweep

--- experiments/sentiment_erasure.py
+++ experiments/sentiment_erasure.py
@@ -2,7 +2,7 @@ import argparse
 import os
 import pickle
 
-from concept_erasure import OracleFitter, QuadraticFitter
+from concept_erasure import LeaceFitter, OracleFitter, QuadraticFitter
 from datasets import load_dataset
 
 from mdl import MlpProbe, Sweep
@@ -18,25 +18,33 @@ def main(args):
     device = args.device
     n_train = 2**17
     seed = args.seed
-
+    print("Shuffling... ", end="")
     ds_dict = ds_dict.with_format("torch", columns=["embedding", "label"]).shuffle(
         seed=seed
     )
+    print("done.")
     num_classes = ds_dict["train"].features["label"].num_classes
     X_train = ds_dict["train"]["embedding"][:n_train]
     X_train = X_train / X_train.norm(dim=-1, keepdim=True)
     Y_train = ds_dict["train"]["label"][:n_train]
 
-    for erasure_method in ["Q-LEACE", "None", "Linear"]:
+    # for erasure_method in ["Q-LEACE", "None", "Linear", "LEACE"]:
+    for erasure_method in ["None", "LEACE"]:
         print(f"Erasure: {erasure_method}")
 
         if erasure_method != "None":
-            fitter_class = (
-                QuadraticFitter if erasure_method == "Q-LEACE" else OracleFitter
-            )
+            fitter_class = {
+                "Linear": OracleFitter,
+                "Q-LEACE": QuadraticFitter,
+                "LEACE": LeaceFitter,
+            }[erasure_method]
             fitter = fitter_class.fit(X_train, Y_train)
             eraser = fitter.eraser
-            X_train_ = eraser(X_train, Y_train)
+            X_train_ = (
+                eraser(X_train, Y_train)
+                if erasure_method != "LEACE"
+                else eraser(X_train)
+            )
         else:
             X_train_ = X_train.clone()
 
@@ -50,11 +58,13 @@ def main(args):
             probe_kwargs=dict(
                 num_layers=2,
             ),
-            fit_kwargs=dict(
-                max_epochs=1000,
-            ),
         )
-        result = sweep.run(X_train_.to(device), Y_train.to(device).to(float), seed=seed)
+        result = sweep.run(
+            X_train_.to(device),
+            Y_train.to(device).to(float),
+            seed=seed,
+            max_epochs=1000,
+        )
         print(result)
         out_path = os.path.join(
             args.out_dir, f"{erasure_method}_seed{seed}_on_{ds_name.split('/')[-1]}.pkl"
